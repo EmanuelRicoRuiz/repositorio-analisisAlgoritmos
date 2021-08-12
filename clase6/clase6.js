c7ba3456571b80604a406f090ee381f2ade05e1f
1 <1>
2021-08-12 07:29:28 -0500

    clase6

---
+++ clase6/clase6.js
@@ -0,0 +1,12 @@
+const numero=(num)=>{
+    return num
+}
+const cuadrado2=(num)=>{
+    return (num**2)+2
+}
+console.time("num1")
+console.log(numero(8));
+console.timeEnd("num1")
+// console.time("num2")
+// console.log(cuadrado2(8));
+// console.timeEnd("num2")
