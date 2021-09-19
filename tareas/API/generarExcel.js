cfd00d582edbe9bed91a3301e5d4ba48f2516be1
unknown <emanuelrico1234@gmail.com>
2021-09-19 16:54:07 -0500

    API

--- tareas/API/generarExcel.js
+++ tareas/API/generarExcel.js
@@ -1,6 +1,4 @@
-
 var fs = require('fs');
-
 const controladorVentas=require('./controllers/ventas.controller')
 controladorVentas.consultarVentas().then(respuesta_controlador=>{
     let merge=require('./organizarVentas');
@@ -9,23 +7,14 @@ controladorVentas.consultarVentas().then(respuesta_controlador=>{
 }).catch(error=>{
     console.log('error ',error);
 })
-
 const crearExcel = (lista) => {
-    
     var writeStream = fs.createWriteStream("ventas.xls");
     var header = "Numero factura" + "\t" + " Cliente" + "\t" + "Valor" + "\t"+"fecha" + "\t"+"Vendedor" + "\t"+"Plazo" + "\n";
     writeStream.write(header);
     for (let size=0;size<lista.length;size++){
         writeStream.write(`${lista[size].numerofactura}\t${lista[size].cliente}\t${lista[size].valor}\t${lista[size].fecha}\t${lista[size].vendedor}\t${lista[size].plazo}\n`)
     }
-    
     writeStream.close();
 }
-const filas=(lista, rows, size)=>{
-    if(size==0){
-        return rows
-    }
-    rows.push(`${lista[size].numerofactura}\t${lista[size].cliente}\t${lista[size].valor}\t${lista[size].fecha}\t${lista[size].vendedor}\t${lista[size].plazo}\n`)
-    return filas(lista,rows,size-1)
-}
+
 module.exports={crearExcel}
--- tareas/API/scripts/db.sql
+++ tareas/API/scripts/db.sql
@@ -5,13 +5,10 @@ CREATE TABLE public.ventas (
 	fecha date NOT NULL,
 	vendedor varchar NOT NULL,
 	plazo varchar NOT NULL,
-	CONSTRAINT ventas_pk PRIMARY KEY (numerofactura)
 );
 CREATE TABLE public.articulos (
 	numerofactura int4 NOT NULL,
 	codprod varchar NOT NULL,
 	nombreprod varchar NOT NULL,
 	precio int4 NOT NULL,
-	CONSTRAINT articulos_pk PRIMARY KEY (numerofactura,codprod),
-	CONSTRAINT articulos_fk FOREIGN KEY (numerofactura) REFERENCES public.ventas(numerofactura)
 );
